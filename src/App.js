957c1538daf8231e531b43238dda6d39cfd42e95
danflynndev <danflynndev@gmail.com>
2022-08-31 16:16:58 -0400

    ### Added MUI and layouts
    this is a **multiline** commit message. I did it

--- src/App.js
+++ src/App.js
@@ -1,51 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Register from './components/Register';
 
-/* ***** TODO ***** */
-/* 
-- get User data, get Matches
-  - proccess and pass into child components
-
-- figure out conditional DB config
-
-- set up MaterialUI
-
-- profile display action
-
-- db connection and commands... how to?
-
-NOTES:
-
-db = process.env == 'prod' ? prodDB : devDB;
-
-fork, git clone, make changes, git add, git commit, git push, pull request
-- all of these have flags that modify behavior
-
-- i made some changes to test pulling a remote repo
-
-*/ 
 
 
 function App() {
-
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <Register></Register>
+    );
+  }
+  
+  export default App;
+  
+  /* ***** TODO ***** */
+  /* 
+  - get User data, get Matches
+    - proccess and pass into child components
+  
+  - figure out conditional DB config
+  
+  - set up MaterialUI
+  
+  - profile display action
+  
+  - db connection and commands... how to?
+  
+  NOTES:
+  
+  db = process.env == 'prod' ? prodDB : devDB;
+  
+  fork, git clone, make changes, git add, git commit, git push, pull request
+  - all of these have flags that modify behavior
+  
+  - i made some changes to test pulling a remote repo
+  
+  */ 
