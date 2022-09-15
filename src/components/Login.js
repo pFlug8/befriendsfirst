d1080895ba42f2818b7c3e28967f33e8a6039b81
Parker Hassenpflug <hassenpflugp@gmail.com>
2022-09-15 12:26:20 -0400

    Updated Login and Landing

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,27 @@
-import { NavLink } from  "react-router-dom";
-
+import React from 'react'
+import { Grid, Paper, TextField, Button, Typography, Link, Stack } from '@mui/material'
 
 export const Login = () => {
-    return (
-        <div>
-            <h1>This is a placeholder login page.</h1>
-            <NavLink to="/register">Register</NavLink>
-        </div>
+    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
+
+    const btnstyle={margin:'8px 0'}
+    return(
+        <form>
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+            <Stack 
+                spacing={2}
+                sx={{
+                }}>
+                <TextField label='Username' variant="outlined" fullWidth required/>
+                <TextField label='Password' type='password' variant="outlined" fullWidth required/>
+                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                <Typography >
+                    <Link href="/register"> Create an Account</Link>
+                </Typography>
+            </Stack>
+            </Paper>
+        </Grid>
+        </form>
     )
 }
